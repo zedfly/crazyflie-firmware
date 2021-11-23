fb683e3bdca92a25b9362408d7c30a6dbfe656f3
gemenerik <rikjbouwmeester@gmail.com>
2021-11-23 14:07:47 +0100

    Add documentation for public ESP flashing functions

--- src/drivers/esp32/interface/esp_rom_bootloader.h
+++ src/drivers/esp32/interface/esp_rom_bootloader.h
@@ -37,12 +37,48 @@
 #define ESP_PARTITION_ADDRESS 0x8000
 #define ESP_FW_ADDRESS 0x10000
 
+/**
+* @brief Called to sync with the bootloader. Allows the ESP to automatically configure the baud rate.
+*
+* @param *sendBuffer Pointer to a buffer used to construct the sync packet. Can be left empty.
+*
+* @return true if sync was successful, false otherwise.
+**/
 bool espRomBootloaderSync(uint8_t *sendBuffer);
 
+/**
+* @brief Called to initialize the flashing with the ESP.
+*
+* @param *sendBuffer Pointer to a buffer used to construct the flash begin packet. Can be left empty.
+* @param numberOfDataPackets The number of data packets that will be sent
+* @param firmwareSize The total to be flashed size
+* @param flashOffset The offset in flash where flashing will start
+*
+* @return true if flash begin command was accepted by ESP, false otherwise.
+**/
 bool espRomBootloaderFlashBegin(uint8_t *sendBuffer, uint32_t numberOfDataPackets, uint32_t firmwareSize, uint32_t flashOffset);
 
+/**
+* @brief Called to flash a packet onto the ESP.
+*
+* @param *sendBuffer Pointer to a buffer used to construct the slip packet. Must contain actual to be flashed data.
+* @param flashDataSize Size of the current data packet
+* @param sequenceNumber The sequence number of the current data packet, indicates this is the nth packet
+*
+* @return true if data was succesfully flashed to ESP, false otherwise.
+**/
 bool espRomBootloaderFlashData(uint8_t *sendBuffer, uint32_t flashDataSize, uint32_t sequenceNumber);
 
+/**
+* @brief Called to attach the SPI memory to the ESP. Must be called before issuing any flash command.
+*
+* @param *sendBuffer Pointer to a buffer used to construct the spi attach packet. Can be left empty.
+*
+* @return true if SPI was succesfully attached to ESP, false otherwise.
+**/
 bool espRomBootloaderSpiAttach(uint8_t *sendBuffer);
 
+/**
+* @brief Called to reboot the ESP into bootloader mode.
+**/
 void espRomBootloaderInit();
